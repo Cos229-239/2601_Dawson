1e79aca7e960206004bf5f3978fe679add27b4fc
Dawson <dawsonpmurray@gmail.com>
2026-02-21 05:26:32 -0500

    Move source files into src/ directory to enable bundling; install mapbox-gl and geojson-vt

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const landing = document.getElementById("landing");
+    const mapScreen = document.getElementById("mapScreen");
+    const devButton = document.getElementById("dev");
+
+    devButton.addEventListener("click", () => {
+        landing.style.display = "none";
+        mapScreen.style.display = "block";
+    });
+});
